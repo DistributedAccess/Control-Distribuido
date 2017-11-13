32fcd1bd14f74d6e1caa9c3ae6fa75621749477a
Verriva <echeverri.va@gmail.com>
2017-11-13 11:24:08 -0600

    Agregar Datos Listo

--- RPC/Configuracion_Red.py
+++ RPC/Configuracion_Red.py
@@ -5,17 +5,18 @@ import commands                         #   Para el uso de comandos Linux
 class Configuracion_Red:
 
     #   Variables privadas
-    _User        =   'root'
-    _Password    =   '2010020726Ev'
+    __User        =   'root'
+    __Password    =   '2010020726Ev'
 
     #   Variables publicas
-    cnx         =   ''          #   Objeto para la Base de Datos
-    Ip          =   ''          #   Direccion Ip
-    Id          =   ''          #   Numero de Id
-    Grupo       =   ''          #   Grupo del Host
-    Tipo_Host   =   ''          #   Tipo de Host, Cliete o Servidor
-    Coordinador =   ''          #   Bandera del Coordinador
-    Busy        =   ''          #   Bnadera del estado
+    cnx         =   None          #   Objeto para la Base de Datos
+    cursor      =   None          #   Cursor para la insertar datos
+    Ip          =   None          #   Direccion Ip
+    Id          =   None          #   Numero de Id
+    Grupo       =   None          #   Grupo del Host
+    Tipo_Host   =   None          #   Tipo de Host, Cliete o Servidor
+    Coordinador =   None          #   Bandera del Coordinador
+    Busy        =   None          #   Bnadera del estado
 
     def __init__(self, user, password):
         #   Esta funcion puede ser considerada como un
@@ -27,8 +28,8 @@ class Configuracion_Red:
 
         #   Se actualizan las variables privadas para
         #   el inicio de sesion de la Base de Datos
-        self._User = user
-        self._Password = password
+        self.__User = user
+        self.__Password = password
 
         #   Se conecta a la base de datos, si hay algun
         #   error en la conexion este te avisara.
@@ -36,6 +37,7 @@ class Configuracion_Red:
             self.cnx = mysql.connector.connect(user = user, password = password,
                                           host = '127.0.0.1',
                                           database = 'TablaRuteo')
+
             print("Conexion establecida a la Base de Datos")
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -48,7 +50,7 @@ class Configuracion_Red:
             self.cnx.close()
 
     def Conec_DB(self):
-        #   Esta funcion conectara el objeto a la Base de datos
+        #   Esta funcion conectara el objeto a la Base de Datos
         #   por primera vez si no se han modificado las variables
         #   _User y _Password o cada que halla una desconexion
         #   hacia ella. Si el las variables _User y _Password
@@ -56,9 +58,10 @@ class Configuracion_Red:
         #   __init__(self, user, password)
 
         try:
-            self.cnx = mysql.connector.connect(user = self._User, password = self._Password,
+            self.cnx = mysql.connector.connect(user = self.__User, password = self.__Password,
                                           host = '127.0.0.1',
                                           database = 'TablaRuteo')
+
             print("Conexion establecida a la Base de Datos")
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -74,27 +77,44 @@ class Configuracion_Red:
         #   Esta funcion desconectara el objeto de la
         #   Base de datos.
         self.cnx.close()
+        self.cursor.close()
         print("Desconexion de la Base de Datos")
 
     def Agregar(self):
+        #   Esta funcion agrega a la Base de Datos la direccion
+        #   Ip del Host, asegurandose que la direccion sea unica
+        #   en la tabla de ruteo
 
-        cursor = self.cnx.cursor()
+        #   Es necesario volver a conectarse con la base de datos
+        #   para poder ingresar datos en las tablas
+        self.cnx = mysql.connector.connect(user = self.__User, password = self.__Password,
+                                      host = '127.0.0.1',
+                                      database = 'TablaRuteo')
+        print("Conexion establecida a la Base de Datos")
+
+        #   Se crea un objeto que que utilice el metodo cursor()
+        #   de mysql para poder ingresar, consultar, eliminar etc. datos
+        self.cursor = self.cnx.cursor()
+
+        A = 'Grupo A'
+        Ip = self.Ip_Host()
+        Host_me = (A, Ip)
+
         Agregar_Host = ("INSERT INTO Ruteo (Grupo, IP)"
                         "VALUES(%s, %s)")
 
-        A = 'Grupo A'
-        Ip = Ip_Host()
+        Eliminar_Host =  ("DELETE FROM Ruteo WHERE IP = '%s'" % Ip)
 
-        Host_me = (A, Ip)
+        self.cursor.execute(Eliminar_Host)
+        print("Se ha eliminado una direccion")
 
-        cursor.execute(Agregar_Host, Host_me)
-
+        self.cursor.execute(Agregar_Host, Host_me)
         print("Se ha agregado una direccion")
 
         self.cnx.commit()
-        cursor.close()
+        self.cursor.close()
 
-    def Ip_Host():
+    def Ip_Host(self):
         #   Esta funcion regresa la direccion ip del Host
         #   a traves de la libreria commands se ejecuta
         #   el programa hostname de Linux y lo que regresa
