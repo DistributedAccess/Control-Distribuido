ee18b9e76b52a46b3ebfc5bc9c36b4f74086a6a6
Verriva <echeverri.va@gmail.com>
2017-11-25 22:20:42 -0600

    Deteccion de ERROR THE BEGININ

--- RPC/Control_Distribuido.py
+++ RPC/Control_Distribuido.py
@@ -1,4 +1,8 @@
 from Configuracion_Red import *
+import threading
+import time
+
+import xmlrpclib
 
 class Control_Distribuido:
 
@@ -44,6 +48,55 @@ class Control_Distribuido:
         print("Memoria_C")
         return "OK"
 
-    def Detec_Falla(self):
-        print("Detec_Falla")
-        return "OK"
+    def Detec_Falla(self, Coordinador,Hello):
+
+        if(Hello == "Hello"):
+            #   Este if se usa cuando un CLIENTE o COORDINADOR
+            #   externo desee supervisar la conexion con este
+            #   host. De modo que si recibe un Hello este if
+            #   responde con un OK
+            return "OK"
+
+        if(Coordinador == 1)
+
+            Contador = 0
+            while(True):
+
+                Respuesta = None
+                Inicio = time.time()
+                Fin = None
+
+                while(Fin <= 5):
+                    Fin = time.time()
+                    Fin = Fin-Inicio
+
+                    if(Respuesta == "OK"):
+                        #   ESTE IF SE ENCARGA DE ENVIAR LA
+                        #   SOLICITUD DEL HELLO Y RECIBIR
+                        #   RESPUESTA
+                        pass
+
+                    else:
+                        #   SI LA RESPUESTA HA SIDO IGUAL A "OK"
+                        #   YA NO SE VULEVE A HACER UNA SOLICITUD
+                        #   POR LO TANTO SE ESPERA A QUE TERMINE EL
+                        #   TIEMPO LIMITE.
+                        pass
+
+                if(Respuesta == "OK")
+                        #   SI LA RESPUESTA FUE OK SE REINICIA EL
+                        #   CONTADOR EN CASO DE TENER ALGUN DATO.
+                        #   SE REINICIA EL ALGORITMOS DE DETECCION
+                        #   DE ERRORES
+                    Contador = 0
+
+                if(Contador < 3):
+                        #   EN CASO DE NO HABER OBTENIDO RESPUESTA
+                        #   SE INCREMENTA EN 1 AL CONTADOR Y EL
+                        #   PROCESO VUELVE A COMENZAR
+                        Contador = Contador + 1
+                else:
+                        #   EL CONTADOR HA SUPERADO EL LIMITE DE
+                        #   TRES DE MODO QUE QUE EL ALGORITMO SALE
+                        #   DEL CICLO WHILE
+                        break
