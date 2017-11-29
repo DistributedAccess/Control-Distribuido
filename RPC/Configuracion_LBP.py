ad55418e3187fcc0c79c279eee2e7f418880514b
Verriva <echeverri.va@gmail.com>
2017-11-29 13:23:50 -0600

    BASES DE DATOS LBP CONFIGURADA!

--- RPC/Configuracion_LBP.py
+++ RPC/Configuracion_LBP.py
@@ -40,7 +40,7 @@ class Configuracion_LBP():
         self.db.commit()
         self.cursor.close()
 
-    def Numero_Usuarios():
+    def Numero_Usuarios(self):
         """ Este metodo regresa el numero de usuarios """
 
         #   DECLARACION DE LOS OBJETOS DE LA BASE DE DATOS
@@ -52,7 +52,25 @@ class Configuracion_LBP():
         QUERY = """SELECT count(ID) FROM USUARIOS"""
 
         self.cursor.execute(QUERY)
-        self.cursor.close()
 
-    def Consultar_Usuarios():
-        pass
+        rows = self.cursor.fetchall()
+        self.cursor.close()
+        return rows
+
+    def Consultar_Usuarios(self, Numero):
+        """ Este metodo regresa los LBP de cada usuarios segun el numero
+            de la fila"""
+
+        self.db = MySQLdb.connect(user = self.__User, passwd = self.__Password,
+                                    host = '127.0.0.1',
+                                    db = 'CONTROL_DISTRIBUIDO')
+        self.cursor = self.db.cursor()
+
+        QUERY = """SELECT LBP_1, LBP_2, LBP_3 FROM USUARIOS WHERE
+                        ID LIMIT %s,1"""
+        Host_me = Numero-1
+
+        self.cursor.execute(QUERY,[Host_me])
+        rows = self.cursor.fetchall()
+        self.cursor.close()
+        return rows
